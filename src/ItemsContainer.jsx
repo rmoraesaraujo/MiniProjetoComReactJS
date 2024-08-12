f778f30f562f3e2252f49a710f8e1b22a61f7365
Renan Araujo <renan290812@gmail.com>
2024-08-12 07:36:01 -0300

    Adicionado função de exibir/remover linhas de separação entre tarefas

--- src/ItemsContainer.jsx
+++ src/ItemsContainer.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import TaskItem from './TaskItem';
 
 const ItemsContainer = ({ listaDeTarefas, removerDaLista }) => {
+  const [checked, setChecked] = useState(false);
   return (
     <div className="items-container">
+      <input
+        type="checkbox"
+        name="border-bottom"
+        id="border-btn"
+        checked={checked}
+        onClick={() => setChecked(!checked)}
+      />
+      <label htmlFor="border-bottom">Habilitar linhas</label>
       {listaDeTarefas.map((t, idx) => (
         <TaskItem
           tarefa={t}
           removerDaLista={removerDaLista}
           key={`task-item-${idx}`}
+          showBorderBottom={checked}
         />
       ))}
     </div>
--- src/TaskItem.jsx
+++ src/TaskItem.jsx
@@ -1,6 +1,6 @@
-const TaskItem = ({ tarefa, removerDaLista }) => {
+const TaskItem = ({ tarefa, removerDaLista, showBorderBottom }) => {
   return (
-    <div className="task-item">
+    <div className={`task-item ${showBorderBottom ? 'bottom-border' : ''}`}>
       <p>{tarefa}</p>
       <button onClick={() => removerDaLista(tarefa)}>Apagar</button>
     </div>
